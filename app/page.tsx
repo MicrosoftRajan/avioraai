e986b2245776cdfe3e2ed60e428afa0c96677677
MicrosoftRajan <rajanvimla27@gmail.com>
2026-05-08 00:34:24 +0530

    Updated landing page and fix miidleware issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import HomePage from "@/components/home/HomePage";
 import NeobrutalLanding from "@/components/landing/NeobrutalLanding";
-import { RETURNING_VISITOR_COOKIE } from "@/lib/first-visit";
 import { currentUser } from "@clerk/nextjs/server";
-import { cookies } from "next/headers";
 
 export default async function Page() {
   const user = await currentUser();
@@ -10,14 +8,9 @@ export default async function Page() {
     return <HomePage />;
   }
 
-  const jar = await cookies();
-  if (!jar.get(RETURNING_VISITOR_COOKIE)) {
-    return (
-      <main className="mx-0 flex w-full max-w-none flex-col gap-0 bg-transparent px-0 pt-0">
-        <NeobrutalLanding />
-      </main>
-    );
-  }
-
-  return <HomePage />;
+  return (
+    <main className="mx-0 flex w-full max-w-none flex-col gap-0 bg-transparent px-0 pt-0">
+      <NeobrutalLanding />
+    </main>
+  );
 }
